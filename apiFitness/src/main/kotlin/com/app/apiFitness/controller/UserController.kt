1026fad70022d297fcc9b6de24923ee5789cca51
tiagolobs <tiagolobs@gmail.com>
2020-01-08 22:06:56 -0300

    teste2

--- apiFitness/src/main/kotlin/com/app/apiFitness/controller/UserController.kt
+++ apiFitness/src/main/kotlin/com/app/apiFitness/controller/UserController.kt
@@ -4,7 +4,7 @@ package com.app.apiFitness.controller
 import com.app.apiFitness.entity.User
 import com.app.apiFitness.repository.UserRepository
 import org.springframework.web.bind.annotation.*
-//teste
+
 @RestController
 @RequestMapping("/api")
 class LivroController(private val userRepository: UserRepository) {
