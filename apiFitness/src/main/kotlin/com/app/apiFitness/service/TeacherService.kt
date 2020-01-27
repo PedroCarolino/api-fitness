59da7f6c7573449dbe105e95588c55c7d9beca19
tiagolobs <tiagolobs@gmail.com>
2020-01-27 00:23:35 -0300

    consulta do email na criação do usuario

--- apiFitness/src/main/kotlin/com/app/apiFitness/service/TeacherService.kt
+++ apiFitness/src/main/kotlin/com/app/apiFitness/service/TeacherService.kt
@@ -7,10 +7,15 @@ import com.app.apiFitness.database.repository.UserRepository
 import com.app.apiFitness.database.repository.entity.StudentEntity
 import com.app.apiFitness.database.repository.entity.TeacherEntity
 import com.app.apiFitness.database.repository.entity.UserEntity
+import com.app.apiFitness.exceptions.BusinessException
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Required
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
+import java.lang.RuntimeException
+import javax.persistence.NonUniqueResultException
 
 @Service
 class TeacherService {
@@ -26,9 +31,11 @@ class TeacherService {
     @Autowired
     private lateinit var bCryptPasswordEncoder: BCryptPasswordEncoder
 
-
+    //@Transactional(rollbackFor = [NonUniqueResultException::class])
+    //@Transactional(propagation=Propagation.NEVER)
     fun create(user: UserRequestDTO) {
-        createProfessor(createUser(user).id)
+        consultarEmail(user.email)
+        createStudentUser(user)
     }
     private fun createUser(user: UserRequestDTO): UserEntity {
         var userEntity = UserEntity(user);
@@ -39,11 +46,13 @@ class TeacherService {
         var studentEntity = StudentEntity(userId);
         return studentRepository.save(studentEntity)
     }
-
-    private fun createProfessor(userId: Int?): TeacherEntity {
-        var teacherEntity = TeacherEntity();
-        teacherEntity.userId = userId
-        teacherEntity.CREF = "Teste"
-        return teacherRepository.save(teacherEntity)
+    private fun createStudentUser(user: UserRequestDTO) {
+        createStudent(createUser(user).id)
+    }
+    private fun consultarEmail(userEmail: String?) {
+        var user = userRepository.findByEmail(userEmail)
+        if (user != null){
+            throw BusinessException("O email passado já está sendo utilizado")
+        }
     }
 }
