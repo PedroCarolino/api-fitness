b791c4e3d948a8561f855e252268dddcef45203f
PedroCarolino <pedrocarolino@live.com>
2020-02-17 22:35:05 -0300

    cors config

--- apiFitness/src/main/kotlin/com/app/apiFitness/config/SwaggerConfig.kt
+++ apiFitness/src/main/kotlin/com/app/apiFitness/config/SwaggerConfig.kt
@@ -2,18 +2,16 @@ package com.app.apiFitness.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 import org.springframework.context.annotation.Profile
 import org.springframework.web.bind.annotation.RequestMethod
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.builders.ResponseMessageBuilder
-import springfox.documentation.schema.ModelRef
 import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
-import java.util.*
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @Configuration
 @EnableSwagger2
@@ -25,5 +23,13 @@ class ApiDocumentationConfig {
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.any())
-            .build()
+            .build().apiInfo(apinfo())
+
+    private fun apinfo(): ApiInfo? {
+        return ApiInfoBuilder()
+                .title("User apiFitness")
+                .description("Rest ApiFitness ")
+                .version("1.0.0")
+                .build()
+    }
 }
