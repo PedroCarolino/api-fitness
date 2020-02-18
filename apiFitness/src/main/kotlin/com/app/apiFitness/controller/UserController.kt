16d34917501c28e2eb794f676d60949a7d3a7033
PedroCarolino <pedrocarolino@live.com>
2020-02-18 15:07:36 -0300

    config swagger

--- apiFitness/src/main/kotlin/com/app/apiFitness/controller/UserController.kt
+++ apiFitness/src/main/kotlin/com/app/apiFitness/controller/UserController.kt
@@ -1,5 +1,6 @@
 package com.app.apiFitness.controller
 
+import com.app.apiFitness.controller.dto.request.TeacherProfileRequestDTO
 import com.app.apiFitness.controller.dto.request.UserProfileRequestDTO
 import com.app.apiFitness.controller.dto.response.StandardResponseDTO
 import com.app.apiFitness.service.UserService
@@ -14,12 +15,23 @@ import java.net.URI
 
 @Controller
 @RequestMapping("/create")
-open class UserController @Autowired constructor(private val userService: UserService) {
+class UserController @Autowired constructor(private val userService: UserService,
+                                            private val teacherService: UserService) {
 
     @PostMapping(value = ["/createUser"])
     fun createUserProfile(@RequestBody userProfileRequestDTO: UserProfileRequestDTO): ResponseEntity<StandardResponseDTO>{
         try {
             userService.create(userProfileRequestDTO)
+        } catch (ex : Exception){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(StandardResponseDTO(1, "500"))
+        }
+        return ResponseEntity.created(URI("")).body(StandardResponseDTO(0,"200"))
+    }
+
+    @PostMapping(value=["/Teacher"])
+    fun createTeacherProfile(@RequestBody teacherProfileRequestDTO: TeacherProfileRequestDTO): ResponseEntity<StandardResponseDTO> {
+        try {
+            teacherService.create(teacherProfileRequestDTO)
         } catch (ex : Exception){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(StandardResponseDTO(1, "500"))
         }
